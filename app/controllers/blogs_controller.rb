d18e3186c7ddfcdfa28f47f9d658af5adb0dede5
prinny0142 <piano.hatsune.lesson@gmail.com>
2020-04-14 18:22:36 +0900

    [rails]exercise

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,26 @@
+class BlogsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    blog = Blog.new
+    blog.save
+    redirect_to blog_path
+  end
+
+  def edit
+  end
+
+private
+  def blog_params
+    params.require(:blog).permit(:title, :category, :body)
+  end
+  
+end

d86a6d87b619a47934568b850178f66ea07cd398
prinny0142 <piano.hatsune.lesson@gmail.com>
2020-04-14 20:02:33 +0900

    [finish]rails

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,8 +1,10 @@
 class BlogsController < ApplicationController
   def index
+    @blogs = Blog.all
   end
 
   def show
+    @blog = Blog.find(params[:id])
   end
 
   def new
@@ -10,12 +12,25 @@ class BlogsController < ApplicationController
   end
 
   def create
-    blog = Blog.new
+    blog = Blog.new(blog_params)
     blog.save
-    redirect_to blog_path
+    redirect_to blog_path(blog.id)
   end
 
   def edit
+    @blog = Blog.find(params[:id])
+  end
+
+  def update
+    blog = Blog.find(params[:id])
+    blog.update(blog_params)
+    redirect_to blog_path(blog)
+  end
+
+  def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy
+    redirect_to blogs_path
   end
 
 private
